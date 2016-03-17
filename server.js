3fa177b4d5fd3bd5ee180ecff0fb05b7d0a7a1a8
Florian Edelmaier <florian.edelmaier@gmail.com>
2016-03-18 00:09:46 +0100

    update packages

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 
-var port = process.env.PORT || 81;
+var port = 81;
 app.listen(port, function() {
 	console.log("Jugendsommer is listening on port " + port + " ...");
 });
