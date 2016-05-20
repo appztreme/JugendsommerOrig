82ed8a234821b70f421979d339e4caef597189c0
Florian Edelmaier <florian.edelmaier@gmail.com>
2016-05-20 14:55:11 +0200

    commitment report show/hide sections

--- public/src/Commitments/commitmentEdit.Ctrl.js
+++ public/src/Commitments/commitmentEdit.Ctrl.js
@@ -3,29 +3,43 @@ var app = angular.module('js');
 app.controller('CommitmentEditCtrl', function($scope, $location, NotificationSvc, ActivitiesSvc) {
 	$scope.title = 'Rechnung editieren';
 
-	// $scope.save = function() {
-	// 	if($scope.name) {
-	// 		ActivitiesSvc.create({
-	// 			name: $scope.name,
-	// 			description: $scope.description,
-	// 			startDate: $scope.startDate,
-	// 			endDate: $scope.endDate,
-	// 			maxParticipants: $scope.maxParticipants,
-	// 			queueSize: $scope.queueSize,
-	// 			eventId: ActivitiesSvc.eventId
-	// 		}).success(function(activity) {
-	// 			$scope.name = null;
-	// 			$scope.description = null;
-	// 			$scope.startDate = null;
-	// 			$scope.endDate = null;
-	// 			$scope.maxParticipants = 0;
-	// 			$scope.queueSize = 0;
-	// 		}).then(function() {
-	// 			NotificationSvc.notify('Detail erfolgreich erstellt');
-	// 			$location.path('/activities/' + ActivitiesSvc.eventId);
-	// 		});
-	// 	}
-	// };
+	$scope.save = function() {
+		if($scope.name && $scope.amount && $scope.date) {
+			CommitmentSvc.create({
+				name: $scope.name,
+				type: $scope.type,
+				description: $scope.description,
+				date: $scope.date,
+				amount: $scope.amount,
+				userId: IdentitySvc.currentUser._id,
+				eventId: $routeParams.eventId,
+				isPaymentDone: $scope.isPaymentDone,
+				isPaymentJDDone: $scope.isPaymentJDDone,
+				isInvoice: $scope.isInvoice
+			}).success(function(commitment) {
+				$scope.name = null;
+				$scope.description = null;
+				$scope.date = null;
+				$scope.type = null;
+				$scope.amount = 0;
+				$scope.isPaymentDone = false;
+				$scope.isPaymentJDDone = false;
+				$scope.isInvoice = false;
+			}).then(function() {
+				NotificationSvc.notify('Rechnung erfolgreich editiert');
+				$location.path('/myCommitments');
+			});
+		}
+	};
 
-
+	CommitmentSvc.findById($routeParams.commitmentId).success(function(com) {
+		$scope.name = com.name;
+		$scope.description = com.description;
+		$scope.type = com.type;
+		$scope.amount = com.amount;
+		$scope.date = new Date(com.date);
+		$scope.isPaymentDone = com.isPaymentDone;
+		$scope.isPaymentJDDone = com.isPaymentJDDone;
+		$scope.isInvoice = isInvoice;
+	});
 });
