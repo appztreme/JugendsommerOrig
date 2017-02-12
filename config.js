2d77d1ee5b37633f5615e220f36ab84d82fecb13
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-02-12 21:45:02 +0100

    add new programme locations

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "validLocations": ['Deutschnofen', 'Jenesien', 'Karneid', 'Mölten', 'Neustift', 'Ritten', 'Sarntal', 'SpaceCamp', 'Tiers', 'Welschnofen', 'Hüttenlagerwoche', 'Tschögglberger Jungbläserwoche'],
+    "validLocations": ['Deutschnofen', 'Jenesien', 'Jugenddienst Bozen-Land', 'Jugenddienst Wipptal', 'Karneid', 'Mölten', 'Neustift', 'Ritten', 'Sarntal', 'SpaceCamp', 'Tiers', 'Welschnofen', 'Hüttenlagerwoche', 'Tschögglberger Jungbläserwoche'],
     "cities": ['Deutschnofen', 'Jenesien', 'Karneid', 'Mölten', 'Ritten', 'Sarntal', 'Tiers', 'Vöran', 'Welschnofen', 'Andere']
 }
