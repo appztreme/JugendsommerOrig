d1c511257ab0f36b9b44ad36bdb104b9fd975fc5
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-02-12 21:11:06 +0100

    agb dependent on type

--- public/src/AGB/agbEdit.ctrl.js
+++ public/src/AGB/agbEdit.ctrl.js
@@ -2,17 +2,19 @@ var app = angular.module('js');
 
 app.controller('AgbEditCtrl', function($scope, $routeParams, $location, AgbsSvc, NotificationSvc) {
 	$scope.activityId = $routeParams.activityId;
+	$scope.type = $routeParams.type;
+	// console.log("type:", $routeParams.type);
 
-	$scope.updateAgb = function() {
-		AgbsSvc.update($scope.agb).success(function(agb) {
-		}).then(function() {
-				
-			NotificationSvc.notify('Aenderung erfolgreich gespeichert');
-		});
-	};
+	// $scope.updateAgb = function() {
+	// 	AgbsSvc.update($scope.agb).success(function(agb) {
+	// 	}).then(function() {
+	//
+	// 		NotificationSvc.notify('Aenderung erfolgreich gespeichert');
+	// 	});
+	// };
 
-	AgbsSvc.find().success(function(agbs) {
-		$scope.agb = agbs[0];	
-	});
+	// AgbsSvc.find().success(function(agbs) {
+	// 	$scope.agb = agbs[0];
+	// });
 
 });
