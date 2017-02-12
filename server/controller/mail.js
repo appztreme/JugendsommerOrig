c3e8b3db755b12b1c5e8578efc6567a051f3793a
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-02-12 21:33:15 +0100

    email notification per type

--- server/controller/mail.js
+++ server/controller/mail.js
@@ -4,6 +4,8 @@ var server  = email.server.connect({});
 var htmlStart = "<html><body><p>Die Anmeldung f&uuml;r ";
 var htmlEnd = " f&uuml;r die Sommerprogramme des Jugenddienstes Bozen Land war erfolgreich.</p><p>Einzahlungsschein wird demn&auml;chst mittels email zugesandt.</p><p>Vielen Dank f&uuml;r die Anmeldung.</p></body></html>";
 
+var htmlSpiritnight = "<html><body><p><strong>Vielen Dank</strong> f&uuml;r die Teilnahme an der SpiritNight 2017!</p><p>Bitte best&auml;tigen Sie die Anmeldung bis zum <strong>31.03.2017</strong> mit einer gesammelten &Uuml;berweisung f&uuml;r die gesamte Pfarrei. Die Gesamtsumme kann an folgende Konten &uuml;berwiesen werden:</p><p>S&uuml;dtiroler Volksbank<br />IBAN: IT42C0585658220070571084313<br />SWIFT/BIC: BPAAIT2BBRE</p><p>S&uuml;dtiroler Sparkasse<br />IBAN: IT62J0604558220000000078000<br />SWIFT/BIC: CRBZIT2B050</p><p>Raiffeisenkasse Eisacktal:<br />IBAN: IT95Y0830759090000301223658<br />SWIFT/BIC: RZSBIT21107</p><p>Bitte geben Sie bei der &Uuml;berweisung <strong>Spiritnight 2017 / Name der Pfarrei</strong> an.</p></body></html>";
+
 var txtStart = "Die Anmeldung für ";
 var txtEnd = " für die Sommerprogramme des Jugenddienstes Bozen Land war erfolgreich. Einzahlungsschein wird demnächst mittels email zugesandt.";
 
@@ -14,6 +16,22 @@ function getTypeString(type) {
 		case 'spiritnight': return 'SpiritNight'; break;
 		case 'club': return 'Jugendraum'; break;
 		default: return 'Jugendsommer';
+	}
+}
+
+function getTypeBody(type) {
+	switch (type) {
+		case 'summer':
+		case 'music':
+		case 'club':
+			return htmlStart + firstNameChild + " " + lastNameChild + htmlEnd;
+			break;
+		case 'spiritnight':
+			return htmlSpiritnight;
+			break;
+		default:
+			return htmlStart + firstNameChild + " " + lastNameChild + htmlEnd;
+			break;
 	}
 }
 
