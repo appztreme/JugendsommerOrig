8e0d967eafded4de4976abf0fc527fba1e61cd1d
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-02-26 22:48:32 +0100

    forever startup scripts

---
+++ config.kiso.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "validLocations": [
+        {"name": 'Bozen',            "name_it": 'Bolzano'},
+    ],
+    "cities": ['Bozen', 'Andere'],
+    "db_test": "mongodb://localhost/kiso_test",
+    "db_prod": "mongodb://localhost/kiso",
+    "port": 82
+}
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var app = require('./app');
 var config = require('./config');
+var args = require('args');
 
-app.listen(config.port, function() {
-	console.log("Jugendsommer is listening on port " + config.port + " ...");
+args.option('port', 'The port on which the app will be running', config.port);
+var flags = args.parse(process.argv)
+
+app.listen(flags.port, function() {
+	console.log("Jugendsommer is listening on port " + flags.port + " ...");
 });
