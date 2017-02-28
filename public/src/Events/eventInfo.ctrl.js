20f6aceb51c33fb1bec7bfdd82d9796e324b939b
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-02-28 14:49:12 +0100

    change multi-lang events screen

--- public/src/Events/eventInfo.ctrl.js
+++ public/src/Events/eventInfo.ctrl.js
@@ -1,10 +1,16 @@
 var app = angular.module('js');
 
-app.controller('EventInfoCtrl', function($scope, $routeParams, EventsSvc) {
+app.controller('EventInfoCtrl', function($scope, $routeParams, EventsSvc, $rootScope, $translate) {
 	$scope.busyPromise = EventsSvc.findById($routeParams.eventId);
 
+	$scope.lang = $translate.proposedLanguage() || $translate.user();
+
+    $rootScope.$on('$translateChangeSuccess', function() {
+        $scope.lang = $translate.proposedLanguage() || $translate.user();
+    });
+
 	EventsSvc.findById($routeParams.eventId).success(function(ev) {
-		$scope.event = ev;	
+		$scope.event = ev;
 		$scope.info = ev.info;
 	});
 });
--- public/src/Events/events.ctrl.js
+++ public/src/Events/events.ctrl.js
@@ -1,9 +1,14 @@
 var app = angular.module('js');
 
-app.controller('EventsCtrl', function($scope, $routeParams, $location, EventsSvc, IdentitySvc) {
+app.controller('EventsCtrl', function($scope, $routeParams, $location, EventsSvc, IdentitySvc, $rootScope, $translate) {
 	//$scope.busyPromise = EventsSvc.find();
 	var host = $location.$$host.toLowerCase();
-	//var url = $location.url();
+
+	$scope.lang = $translate.proposedLanguage() || $translate.user();
+
+    $rootScope.$on('$translateChangeSuccess', function() {
+        $scope.lang = $translate.proposedLanguage() || $translate.user();
+    });
 
 	if(IdentitySvc.isAdmin()) {
 		if(['club', 'spiritnight'].indexOf($routeParams.location) !== -1) {
