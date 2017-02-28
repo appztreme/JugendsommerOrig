7cdfb61f3d6f8ed3721b134f59812c9a737ca780
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-02-28 23:50:18 +0100

    distinguish look per mandant

--- public/src/AGB/app.ctrl.js
+++ public/src/AGB/app.ctrl.js
@@ -1,5 +1,7 @@
 var app = angular.module('js');
 
-app.controller('ApplicationCtrl', function($scope, IdentitySvc) {
+app.controller('ApplicationCtrl', function($scope, IdentitySvc, $location) {
 	$scope.iSvc = IdentitySvc;
+	var host = $location.$$host.toLowerCase();
+    $scope.isKiso = host.indexOf('kiso') !== -1;
 });
