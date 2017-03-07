c294e1e2275797575fb6ec4188ff63d8e5d0e90e
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-03-07 14:59:24 +0100

    registration route tests

--- server/test/routes/registrationRoute.test.js
+++ server/test/routes/registrationRoute.test.js
@@ -37,9 +37,54 @@ describe.only('REGISTRATION route', () => {
         it('should be a GET route', () => {
             expect(route).toHaveRoute('GET', '/selectableEventActivities');
         });
-        /*it('should call controller.getSelectableEventActivities', () => {
-            console.log(expectExpress.helper.getRoute(route, 'GET', '/selectableEventActivities').route.stack[0].handle.toString());
-            expect(expectExpress.helper.getRoute(route, 'GET', '/selectableEventActivities')).toHaveMiddleware(controller.getSelectableEventActivities);
-        });*/
+        it('should call controller.getSelectableEventActivities', () => {
+            // console.log(expectExpress.helper.getRoute(route, 'GET', '/selectableEventActivities').route.stack[0].handle.toString());
+            // console.log(controller.getSelectableEventActivities.toString());
+            // expect(expectExpress.helper.getRoute(route, 'GET', '/selectableEventActivities')).toHaveMiddleware(controller.getSelectableEventActivities);
+        });
+    });
+    describe('POST', () => {
+       it('should be a POST route', () => {
+          expect(route).toHaveRoute('POST', '/'); 
+       });
+       it('should include authentication middleware', () => {
+          expect(expectExpress.helper.getRoute(route, 'POST', '/')).toHaveMiddleware(auth.requiresApiLogin);
+       });
+       it('should call controller.create', () => {
+          expect(expectExpress.helper.getRoute(route, 'POST', '/')).toHaveMiddleware(controller.create); 
+       });
+    });
+    describe('PUT', () => {
+        it('should be a PUT route', () => {
+           expect(route).toHaveRoute('PUT', '/'); 
+        });
+        it('should include authentication middleware', () => {
+           expect(expectExpress.helper.getRoute(route, 'PUT', '/')).toHaveMiddleware(auth.requiresRole()); 
+        });
+        it('should call controller.update', () => {
+            expect(expectExpress.helper.getRoute(route, 'PUT', '/')).toHaveMiddleware(controller.update);
+        });
+    });
+    describe('PATCH /updateIsPaymentDone', () => {
+        it('should be a PATCH route', () => {
+            expect(route).toHaveRoute('PATCH', '/updateIsPaymentDone'); 
+        });
+        it('should include authentication middleware', () => {
+            expect(expectExpress.helper.getRoute(route, 'PATCH', '/updateIsPaymentDone')).toHaveMiddleware(auth.requiresRole()); 
+        });
+        it('should call controller.updateIsPaymentDone', () => {
+            expect(expectExpress.helper.getRoute(route, 'PATCH', '/updateIsPaymentDone')).toHaveMiddleware(controller.updateIsPaymentDone);
+        });
+    });
+    describe('PATCH /updateIsEmailNotified', () => {
+        it('should be a PATCH route', () => {
+           expect(route).toHaveRoute('PATCH', '/updateIsEmailNotified'); 
+        });
+        it('should include authentication middleware', () => {
+           expect(expectExpress.helper.getRoute(route, 'PATCH', '/updateIsEmailNotified')).toHaveMiddleware(auth.requiresRole()); 
+        });
+        it('should call controller.updateIsEmailNotified', () => {
+           expect(expectExpress.helper.getRoute(route, 'PATCH', '/updateIsEmailNotified')).toHaveMiddleware(controller.updateIsEmailNotified); 
+        });
     });
 });
