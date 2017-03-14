c334615c33f0643d750651f1f03d5bb75ba4caf7
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-03-14 22:33:37 +0100

    add jdbland config

---
+++ config.jd-bozenland.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "validLocations": [
+        {"name": 'Deutschnofen',            "name_it": 'Nova Ponente'},
+        {"name": 'Jenesien',                "name_it": 'San Genesio'},
+        {"name": 'Jugenddienst Bozen-Land', "name_it": 'Jugenddienst Bozen-Land'},
+        {"name": 'Jugenddienst Wipptal',    "name_it": 'Jugenddienst Wipptal'},
+        {"name": 'Karneid',                 "name_it": 'Cornedo'},
+        {"name": 'Mölten',                  "name_it": 'Meltina'},
+        {"name": 'Neustift',                "name_it": 'Neustift'},
+        {"name": 'Ritten',                  "name_it": 'Renon'},
+        {"name": 'Sarntal',                 "name_it": 'Val Sarentino'},
+        {"name": 'SpaceCamp',               "name_it": 'SpaceCamp'},
+        {"name": 'Tiers',                   "name_it": 'Tires'},
+        {"name": 'Welschnofen',             "name_it": 'Nova Levante'},
+        {"name": 'Hüttenlagerwoche',        "name_it": 'Hüttenlagerwoche'},
+        {"name": 'Tschögglberger Jungbläserwoche', "name_it": 'Tschögglberger Jungbläserwoche'}
+    ],
+    "cities": ['Deutschnofen', 'Jenesien', 'Karneid', 'Mölten', 'Ritten', 'Sarntal', 'Tiers', 'Vöran', 'Welschnofen', 'Andere'],
+    "db_test": "mongodb://localhost/jd_bozenland_test",
+    "db_prod": "mongodb://localhost/jd_bozenland",
+    "port": 3000,
+    "caching": true
+}
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const js_conf = require('./config.jugendsommer');
+const jdbland_conf = require('./config.jd-bozenland');
 const kiso_conf = require('./config.kiso');
 
 module.exports = js_conf;
