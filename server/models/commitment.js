980695ed4a5ddcc93af751550dbd6a0f691d224d
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-04-13 02:07:01 +0200

    set commitment index dependent on cur year

--- server/models/commitment.js
+++ server/models/commitment.js
@@ -2,6 +2,9 @@
 const db = require('../db');
 let mongoose = require('mongoose');
 
+const curYear = new Date().getFullYear();
+const startCurYear = new Date(curYear+"-1-1");
+
 const commitmentSchema = db.Schema({
 	name: { type: String, required: true },
 	index: { type: Number },
@@ -18,22 +21,22 @@ const commitmentSchema = db.Schema({
 	isCleared: { type: Boolean, required: true, default: false },
 });
 
-var Commitment = db.model('Commitment', commitmentSchema);
-
 commitmentSchema.pre('save', function(next) {
   var com = this;
-	if(!com.index > 0) {
-		Commitment.findOne()
+	if(!com.index || !com.index > 0) {
+	Commitment.findOne({date: {$gte: startCurYear}})
     	.sort({index: -1})
     	.exec(function(err, doc)
     	{
-        var max = (doc && doc.index) ? doc.index : 0;
-        com.index = max + 1;
-				next();
+        	var max = (doc && doc.index) ? doc.index : 0;
+        	com.index = max + 1;
+			next();
     	});
 	} else {
 		next();
 	}
 });
+
+var Commitment = db.model('Commitment', commitmentSchema);
 
 module.exports = Commitment;
