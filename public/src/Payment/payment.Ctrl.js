b1c88586a38a5250b46a3054267fb73d31f3bb8a
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-06-09 00:06:40 +0200

    filter for payment report

--- public/src/Payment/payment.Ctrl.js
+++ public/src/Payment/payment.Ctrl.js
@@ -5,11 +5,26 @@ app.controller('PaymentCtrl', function($scope, IdentitySvc, PaymentSvc) {
 
 	$scope.relations = [];
 
+	$scope.clearNameSelection = function() {
+		$scope.nameFilter = '';
+		$scope.relations = [];
+	}
+
 	$scope.getReportData = function() {
+		$scope.relations = [];
 		PaymentSvc.find()
 			.success(function(result) {
-				$scope.relations = result;
-				console.log("result", result);
+				if($scope.nameFilter) {
+					for(var i = 0; i < result.length; i++) {
+						if(result[i].lastName == $scope.nameFilter) {
+							$scope.relations.push(result[i]);
+						}
+					}
+				} else {
+					$scope.relations = result;
+				}		
+
+				// console.log("result", result);
 				// $scope.emails = _.uniq(_.map($scope.payments, function(r) {
                 // 		return r.email;
             	// 	})).join(';');
