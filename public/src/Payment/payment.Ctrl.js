7e151ced6d9d195e8ea0bf9ebaff11846a37d768
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-06-08 23:53:27 +0200

    payment report

--- public/src/Payment/payment.Ctrl.js
+++ public/src/Payment/payment.Ctrl.js
@@ -3,10 +3,12 @@ var app = angular.module('js');
 app.controller('PaymentCtrl', function($scope, IdentitySvc, PaymentSvc) {
 	$scope.title = 'Auszahlung';
 
+	$scope.relations = [];
+
 	$scope.getReportData = function() {
 		PaymentSvc.find()
 			.success(function(result) {
-				// this.scope.payments = result;
+				$scope.relations = result;
 				console.log("result", result);
 				// $scope.emails = _.uniq(_.map($scope.payments, function(r) {
                 // 		return r.email;
--- server/repositories/contact.js
+++ server/repositories/contact.js
@@ -36,40 +36,76 @@ exports.search = (searchToken) => {
 		.exec();
 }
 
-exports.findRelationsForCurrentYear = () => {
-    return Event.aggregate([
-        { $match:
-			{ $and: [ {startDate: { $gte: startCurYear }} ] }
-		},
-        { $lookup:
-            { from: 'activities', localField: '_id', foreignField: 'eventId', as: 'activities' }
-        },
-        // { $unwind: '$activities.contactRels' },
-        // { $lookup:
-        //     { from: 'contacts', localField: 'activities.contactRels.contact', foreignField: '_id', as: 'activities.contactRels.cont' }
-        // },
-        { $unwind: '$contactRels' },
-        { $lookup:
-            { from: 'contacts', localField: 'contactRels.contact', foreignField: '_id', as: 'contactRels.cont' }
-        },
-        { $project:
-            {
-                _id: 0,
-                eventName: { $concat: ["location", " ", "name"] },
-                "contactRels.contact": { $arrayElemAt: ["$contactRels.cont",0] },
-                "activities._id": 1,
-                "activities.name": 1,
-                "activities.contactRels": 1
-            }
-        },
-        // { $lookup:
-        //     { from: 'contacts', localField: 'contactRels.contact', foreignField: '_id', as: 'c' }
-        // }
-    ]).exec();
-    // return Activity.find()
-    //     .where('startDate').gte(startCurYear)
-    //     .populate('contactRels.contact')
-    //     .populate({ path: 'eventId', select: 'location name contactRels', populate:{ path: 'contactRels.contact' } })
-    //     .select('eventId name contactRels')
-    //     .exec();
+const getAmountForRole = (role) => {
+    switch(role) {
+        case 'TeamleiterIn': return 475;
+        case 'TeamleiterIn 24h': return 575;
+        case 'TeamleiterIn 0.5': return 440;
+        case 'TeamleiterIn 24h 0.5': return 540;
+        case 'BetreuerIn': return 400;
+        case 'BetreuerIn 24h': return 500;
+        case 'PraktikantIn': return 200;
+        case 'PraktikantIn 24h': return 300;
+        case 'Andere': return 0;
+        default: return 0;
+    }
+}
+
+const relationComparer = (a, b) => {
+    if(a.lastName < b.lastName) return -1;
+    if(a.lastName > b.lastName) return 1;
+    if(a.firstName > b.firstName) return -1;
+    if(a.firstName < b.firstName) return 1;
+    if(a.startDate > b.startDate) return -1;
+    if(a.startDate < b.startDate) return 1;
+    return 0;
+}
+
+exports.findRelationsForCurrentYear = async () => {
+    let result = [];
+
+    const eventContactRels = await Event.find()
+        .where('startDate').gte(startCurYear)
+        .populate({ path: 'contactRels.contact' })
+        .select('name location contactRels startDate endDate')
+        .exec();
+    const activityContactRels = await Activity.find()
+        .where('startDate').gte(startCurYear)
+        .populate({ path: 'contactRels.contact' })
+        .populate({ path: 'eventId', select: 'location name' })
+        .select('name contactRels eventId startDate endDate')
+        .exec();
+    eventContactRels.forEach(v => {
+        v.contactRels.forEach(vC => {
+            result.push({
+                eventName: v.location + ' ' + v.name,
+                activityName: '',
+                type: vC.role,
+                amount: getAmountForRole(vC.role),
+                firstName: vC.contact.firstName,
+                lastName: vC.contact.lastName,
+                email: vC.contact.email,
+                phoneNumber: vC.contact.phoneNumber,
+                startDate: v.startDate,
+                endDate: v.endDate
+            });
+        })
+    })
+    activityContactRels.forEach(v => {
+        v.contactRels.forEach(vC => {
+            result.push({
+                eventName: v.eventId.location + ' ' + v.eventId.name,
+                activityName: v.name,
+                type: vC.role,
+                amount: getAmountForRole(vC.role),
+                firstName: vC.contact.firstName,
+                lastName: vC.contact.lastName,
+                email: vC.contact.email,
+                phoneNumber: vC.contact.phoneNumber,
+                startDate: v.startDate,
+                endDate: v.endDate
+            })
+        })
+    })
+    return result.sort(relationComparer);
 }
