c2d01ca25b45ba5fbfd78ef263b46cb5b7e609d5
Florian Edelmaier <florian.edelmaier@gmail.com>
2017-06-09 00:39:09 +0200

    paymant report: email address per role

--- public/src/Payment/payment.Ctrl.js
+++ public/src/Payment/payment.Ctrl.js
@@ -5,13 +5,16 @@ app.controller('PaymentCtrl', function($scope, IdentitySvc, PaymentSvc) {
 
 	$scope.relations = [];
 
+	$scope.sumAmount = 0;
+
 	$scope.clearNameSelection = function() {
 		$scope.nameFilter = '';
 		$scope.relations = [];
+		$scope.sumAmount = 0;
 	}
 
 	$scope.getReportData = function() {
-		$scope.relations = [];
+		$scope.clearNameSelection();
 		PaymentSvc.find()
 			.success(function(result) {
 				if($scope.nameFilter) {
@@ -22,12 +25,27 @@ app.controller('PaymentCtrl', function($scope, IdentitySvc, PaymentSvc) {
 					}
 				} else {
 					$scope.relations = result;
-				}		
+				}
 
-				// console.log("result", result);
-				// $scope.emails = _.uniq(_.map($scope.payments, function(r) {
-                // 		return r.email;
-            	// 	})).join(';');
+				$scope.sumAmount = _.reduce($scope.relations, function(memo, r) {
+					return memo + r.amount;
+				}, 0);
+
+				$scope.emailsLeader = _.trim(_.uniq(_.map($scope.relations, function(r) {
+					if(r.type === 'TeamleiterIn' || r.type === 'TeamleiterIn 24h' || r.type === 'TeamleiterIn 0.5' || r.type === 'TeamleiterIn 24h 0.5') {
+                		return r.email;
+					} else return '#';
+            	})).join(';'), '#;');
+				$scope.emailsCaregiver = _.trim(_.uniq(_.map($scope.relations, function(r) {
+					if(r.type === 'BetreuerIn' || r.type === 'BetreuerIn 24h') {
+                		return r.email;
+					} else return '#';
+            	})).join(';'), '#;');
+				$scope.emailsTrainee = _.trim(_.uniq(_.map($scope.relations, function(r) {
+                	if(r.type === 'PraktikantIn' || r.type === 'PraktikantIn 24h') {
+                		return r.email;
+					} else return '#';
+            	})).join(';'), '#;');
 			});
 	}
 });
