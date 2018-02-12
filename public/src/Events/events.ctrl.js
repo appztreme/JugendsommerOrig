7b460081bb1a6f3c07247822aebd8d563aafcdff
Florian Edelmaier <florian.edelmaier@gmail.com>
2018-02-12 04:59:41 +0100

    change back start date

--- public/src/Events/events.ctrl.js
+++ public/src/Events/events.ctrl.js
@@ -9,7 +9,7 @@ app.controller('EventsCtrl', function($scope, $routeParams, $location, EventsSvc
 
 	$scope.isRegistrationWarningVisible = function() {
 		var d = new Date('2018-02-12');
-		d.setHours(4,57);
+		d.setHours(18,58);
 		var now = Date.now();
 		return now < d.getTime();
 	}
@@ -33,7 +33,7 @@ app.controller('EventsCtrl', function($scope, $routeParams, $location, EventsSvc
 		if(IdentitySvc.isAdmin()) return false;
 		else {
 			var visible = new Date(ev.visibleFrom);
-			visible.setHours(4,57);
+			visible.setHours(18,58);
 			if(Date.now() > visible.getTime()) return false;
 			if(Date.now() > new Date(ev.deadline).getTime()) return false;
 			return true;
