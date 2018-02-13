ba0142181bb2cb396d16b3ff6e3a3e05f2f2cbc7
Florian Edelmaier <florian.edelmaier@gmail.com>
2018-02-13 08:57:45 +0100

    provide jdul config interface

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const js_conf = require('./config.jugendsommer');
 const jdbland_conf = require('./config.jd-bozenland');
 const kiso_conf = require('./config.kiso');
+const jdul_conf = require('./config.jsul');
 
 module.exports = js_conf;
