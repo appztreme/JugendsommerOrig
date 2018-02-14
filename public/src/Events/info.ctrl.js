0e31dd5d21617254e50cd2d8578f369657a29591
Florian Edelmaier <florian.edelmaier@gmail.com>
2018-02-14 11:26:38 +0100

    refactor info section per instance

--- public/src/Events/info.ctrl.js
+++ public/src/Events/info.ctrl.js
@@ -2,10 +2,13 @@ var app = angular.module('js');
 
 app.controller('InfoCtrl', function($scope, $rootScope, $translate, $location, PlatformSvc) {
     $scope.platform = PlatformSvc;
-
+    
 	$scope.lang = $translate.proposedLanguage() || $translate.user();
 
     $rootScope.$on('$translateChangeSuccess', function() {
         $scope.lang = $translate.proposedLanguage() || $translate.user();
     });
+
+    console.log(PlatformSvc, PlatformSvc.isTest(), $scope.lang);
+
 });
