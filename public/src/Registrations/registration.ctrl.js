028c4782834150d0ebdc5771d5b3300042b89120
Florian Edelmaier <florian.edelmaier@gmail.com>
2020-03-03 00:43:35 +0100

    typo kiso school levels

--- public/src/Registrations/registration.ctrl.js
+++ public/src/Registrations/registration.ctrl.js
@@ -89,7 +89,7 @@ app.controller('RegistrationCtrl', function($scope, $routeParams, $filter, $loca
 	$scope.type = $routeParams.type;
 	$scope.cities = PlatformSvc.getCities();
 	$scope.tShirtSizes = conf.tSizes;
-	$scope.schoolLevels = $scope.isKiso ? conf.schoolChild_Kiso : conf.schoolLevels;
+	$scope.schoolLevels = $scope.isKiso ? conf.schoolLevels_Kiso : conf.schoolLevels;
 	$scope.cityChild = PlatformSvc.getDefaultCity();
 	$scope.schoolChild = $scope.schoolLevels.length > 0 ? $scope.schoolLevels[0].name : undefined;
 	$scope.addressChild = " ";
