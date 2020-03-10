7a040f8309f2f7fe00b1280518f01d8b28dfe9b2
Florian Edelmaier <florian.edelmaier@gmail.com>
2020-03-10 22:00:41 +0100

    save prio props

--- dbQueries/saveSchemaRegistration.js
+++ dbQueries/saveSchemaRegistration.js
@@ -5,7 +5,10 @@ const main = async() => {
 
     const registrations = await Registration.find();
     for(let i = 0; i < registrations.length; i++) {
-        registrations[i].save();
+        const reg = registrations[i];
+        reg.isPrioUp = false;
+        reg.isPrioDown = false;
+        reg.save();
     }
     //reg.save();
     console.log(chalk.green("Done"));
