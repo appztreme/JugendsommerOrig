30e258d22bef2b8b5d577903a98d176d528c0a90
Florian Edelmaier <florian.edelmaier@gmail.com>
2020-03-11 01:30:36 +0100

    test prio props

--- server/passport.config.js
+++ server/passport.config.js
@@ -9,7 +9,6 @@ module.exports = function()
     passport.use('local', new LocalStrategy(
 			function(username, password, done) {
 				User.findOne({userName: username}).exec(function(err, user) {
-					console.log("auth:", user)
 					if(user && user.authenticate(password)) {
 						return done(null, user);
 					} else {
