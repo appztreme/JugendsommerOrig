7a92249a575434e165361665c33c559e19602c2e
Florian Edelmaier <florian.edelmaier@gmail.com>
2020-03-20 21:19:06 +0100

    log report events

--- dbQueries/queries.js
+++ dbQueries/queries.js
@@ -1,5 +1,5 @@
   // count of this years registrations
-db.registrations.find({registrationDate: {$gte: new Date(2016,1,1)}}).count();
+db.registrations.find({registrationDate: {$gte: new Date(2020,3,19)}}).count();
 
 // find userName doubles via map / reduce
 var map = function() {
