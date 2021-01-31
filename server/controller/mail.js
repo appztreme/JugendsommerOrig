3d10ab714c59fdf65e9b9514ed0d86587642b85d
Florian Edelmaier <florian.edelmaier@gmail.com>
2021-02-01 00:21:45 +0100

    remove pdf from email

--- server/controller/mail.js
+++ server/controller/mail.js
@@ -8,16 +8,12 @@ exports.sendTxtMail = function(recipient, firstNameChild, lastNameChild, type, a
 		var text = mailbuilder.getTypeText(type, firstNameChild, lastNameChild, activities[0].eventId.location, instance, activities);
         var fromEmail = mailbuilder.getSender(instance);
 		var subjectEmail = mailbuilder.getSubject(instance, type);
-		var pdf = null;
-		if(instance.isJDUL) {
-			pdf = mailbuilder.getReservationAttachment(type, firstNameChild, lastNameChild, activities, reservation, instance);
-		}
 		server.send({
 		text: text,
 		from: fromEmail,
 		to: recipient,
 		subject: subjectEmail,
-		attachment: mailbuilder.getAttachment(body, instance, pdf)
+		attachment: mailbuilder.getAttachment(body, instance)
 	}, function(err, message) {console.log(err||message); });
 };
 
--- server/controller/mailBuilder.js
+++ server/controller/mailBuilder.js
@@ -142,15 +142,16 @@ exports.getTypeText = function(type, firstNameChild, lastNameChild, location, in
 	}
 }
 
-exports.getAttachment = function(body, instance, pdf) {
+exports.getAttachment = function(body, instance) {
 	if(instance.isJDBL || instance.isJugendsommer) {
 		return [{ data: body, alternative: true },
 			    { path:"public/assets/jdbl-logo.jpg", type:"image/jpg", headers:{"Content-ID":"<my-image>"} }]
 	}
 	else if (instance.isJDUL) {
+		// var pdf = getReservationAttachment();
 		return [{ data: body, alternative: true },
-			{ path:"public/assets/jdul_ente.jpg", type:"image/jpg", headers:{"Content-ID":"<my-image>"} },
-		    { stream: pdf, type:"application/pdf", name: 'Bestaetigung.pdf' }]
+			{ path:"public/assets/jdul_ente.jpg", type:"image/jpg", headers:{"Content-ID":"<my-image>"} }]
+		    //{ stream: pdf, type:"application/pdf", name: 'Bestaetigung.pdf' }]
 	} else {
 		return [{ data: body, alternative: true }];
 	}
