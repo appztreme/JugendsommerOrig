4723bbd223a8d329b515a68cdeac6d024630417b
Florian Edelmaier <florian.edelmaier@gmail.com>
2021-02-09 17:26:39 +0100

    new script to store props on Activities

---
+++ dbQueries/setActivitesCurParticipants0.js
@@ -0,0 +1,41 @@
+const Event = require('./../server/models/event');
+const Activity = require('./../server/models/activity');
+const chalk = require('chalk');
+
+const year = 2021;
+const verbose = true;
+
+
+const main = async() => {
+    const events = await Event.find().where('startDate').gte(new Date(`${year}-01-01`)).exec();
+    
+    for(let i = 0; i < events.length; i++) {
+        const ev = events[i];
+
+        const activities = await Activity.find().where('eventId').equals(ev._id);
+        for(let j = 0; j < activities.length; j++) {
+            const act = activities[j];
+            act.curParticipants = 0;
+            act.queueSize = 10;
+
+            
+            if(verbose) {
+                console.log(chalk.cyan(act.name));
+                console.log(chalk.cyan(act.curParticipants, actu.queueSize));
+            }
+           try {
+                await act.save();
+            } catch(eAct) {
+                console.log(chalk.red("error Activity:", eAct))
+            }
+        }
+        if(verbose) {
+            console.log(chalk.red("===================================================="))
+        }
+
+    }
+
+    process.exit(1);
+}
+
+main();
