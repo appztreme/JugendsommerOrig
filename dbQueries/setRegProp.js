d772b2f899ec1ed76ebc7b0db6c79d0d6ccae566
Florian Edelmaier <florian.edelmaier@gmail.com>
2021-03-10 22:45:27 +0100

    create new record for missing data1

--- dbQueries/setRegProp.js
+++ dbQueries/setRegProp.js
@@ -4,7 +4,61 @@ const mongoose = require('mongoose');
 
 const main = async() => {
 
-    let reg = await Registration.findById(mongoose.Types.ObjectId("604815c86cd56d710de28013"));
+    
+    //let reg = await Registration.findById(mongoose.Types.ObjectId("604815c86cd56d710de28013"));
+    let reg = new Registration({
+        covidRules: {
+          isAtRisk: false,
+          isPrioWork: false,
+          isPrioSocial: false,
+          isPrioNone: false
+        },
+        transferStatus: { move: false, unavailable: false, waitlist: false },
+        addressChild: "Piazza Mazzini 8/B",
+        cityChild: "Bozen",
+        hasOwnEBike: false,
+        isPaymentDone: false,
+        isEmailNotified: false,
+        needsPreCare: false,
+        hasHealthIssues: false,
+        hasDisability: false,
+        needsEbK: false,
+        canSwim: false,
+        canGoHomeAllone: false,
+        isSiblingReservation: true,
+        acceptsOptionalFee: false,
+        asksForReduction: false,
+        acceptsNewsletter: false,
+        acceptsMediaPublication: true,
+        isPrioUp: false,
+        isPrioDown: false,
+        isRefunded: false,
+        gender: "male",
+        wasWaiting: false,
+        _id: ObjectId("604815c86cd56d710de28013"),
+        registrationDate: new Date("2021-03-02T06:10:20.000Z"),
+        firstNameParent: "Lukas",
+        lastNameParent: "Steinmair",
+        phoneNumberParent: "3479569832",
+        emailParent: "lukas.steinmair@hotmail.com",
+        firstNameChild: "Benno",
+        lastNameChild: "Steinmair",
+        birthdayChild: new Date("2012-09-10T00:00:00.000Z"),
+        schoolChild: "3. Klasse Grundschule",
+        taxNumber: "STNBNN12P10B220X",
+        nameContact1: "Anna Maria Bogana",
+        telContact1: "3396103134",
+        nameContact2: "Steffi Fink",
+        telContact2: "3396785319",
+        activityId: ObjectId("601029d2fadc13a71e1b944c"),
+        tShirtSize: "9-10 Jahre/134-140cm",
+        preferredFellow: "",
+        healthIllnes: "nein",
+        disabilityDescription: "nein",
+        userId: ObjectId("6034dd98584ebf0334d7a5e1")
+      }) 
+    let valErr = reg.validateSync();
+    console.log("error", valErr);
     reg.save();
     
     //console.log(chalk.green("number registrations:", registrations.length));
