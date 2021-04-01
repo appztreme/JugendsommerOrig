ed9f52e689f4046f5ac8f5754e452329734240cb
Florian Edelmaier <florian.edelmaier@gmail.com>
2021-04-01 10:38:43 +0200

    change db script

--- dbQueries/setRegProp.js
+++ dbQueries/setRegProp.js
@@ -1,13 +1,14 @@
 const Registration = require('./../server/models/registration');
 const chalk = require('chalk');
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const main = async() => {
 
     
     let reg = await Registration.findById(mongoose.Types.ObjectId("603f28d96abc3576b0bd223b")); //603f28d96abc3576b0bd223c
-    reg.registrationDate.setMonth(2);
-    reg.registrationDate.setDate(20);
+    let newDate = moment(reg.registrationDate).add(-1, 'months').setDate(20);
+    reg.registrationDate = newDate;
     let valErr = reg.validateSync();
     console.log("error", valErr);
     await reg.save();
