13bc57d486cb13fa17909be20189290356b2a52c
Florian Edelmaier <florian.edelmaier@gmail.com>
2021-05-21 00:18:44 +0200

    adapt cur participants

--- dbQueries/moveRegs.js
+++ dbQueries/moveRegs.js
@@ -1,4 +1,5 @@
 const Registration = require('./../server/models/registration');
+const Activity = require('./../server/models/activity')
 const chalk = require('chalk');
 const mongoose = require('mongoose');
 
@@ -6,17 +7,26 @@ const main = async() => {
     const fromActivityId = mongoose.Types.ObjectId("6007f11584f0e58db88c3af7");
     const toActivityId = mongoose.Types.ObjectId("6007f1605747a38d9c9c3dd7");
 
-    const registrations = await Registration.find({activityId: fromActivityId}).exec();
+    const fromActivity = await Activity.findById(fromActivityId);
+    const toActivity = await Activity.findById(toActivityId);
 
-    for(let i = 0; i < registrations.length; i++) {
-        registrations[i].activityId = toActivityId;
-        try {
-            await registrations[i].save();
-            console.log(chalk.green("registration: ", registrations[i]._id))
-        } catch(err) {
-            console.log(chalk.red("error Reg:", err));
-        }
-    }
+    fromActivity.curParticipants = 0;
+    toActivity.curParticipants = 18;
+
+    await fromActivity.save();
+    await toActivity.save();
+
+    // const registrations = await Registration.find({activityId: fromActivityId}).exec();
+
+    // for(let i = 0; i < registrations.length; i++) {
+    //     registrations[i].activityId = toActivityId;
+    //     try {
+    //         await registrations[i].save();
+    //         console.log(chalk.green("registration: ", registrations[i]._id))
+    //     } catch(err) {
+    //         console.log(chalk.red("error Reg:", err));
+    //     }
+    // }
 
     console.log(chalk.green("Done"));
 
