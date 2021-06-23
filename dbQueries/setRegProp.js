b846cd17c28fee958025ab41fe9cf9e4a969bd5b
Florian Edelmaier <florian.edelmaier@gmail.com>
2021-06-24 00:28:26 +0200

    adapt script & extend myReg timespan

--- dbQueries/setRegProp.js
+++ dbQueries/setRegProp.js
@@ -6,9 +6,10 @@ const moment = require('moment');
 const main = async() => {
 
     
-    let reg = await Registration.findById(mongoose.Types.ObjectId("603f28d96abc3576b0bd223b")); //603f28d96abc3576b0bd223c
-    let newDate = moment(reg.registrationDate).add(-1, 'months').setDate(20);
-    reg.registrationDate = newDate;
+    let reg = await Registration.findById(mongoose.Types.ObjectId("602ac72ffbd219b240426984"));
+    reg.activityId = mongoose.Types.ObjectId("6007f2fbe6146a8dab8c9b29");
+    //let newDate = moment(reg.registrationDate).add(-1, 'months').setDate(20);
+    //reg.registrationDate = newDate;
     let valErr = reg.validateSync();
     console.log("error", valErr);
     await reg.save();
--- server/controller/myRegistrationCtrl.js
+++ server/controller/myRegistrationCtrl.js
@@ -6,13 +6,19 @@ const platform = require('./platform');
 const Activity = require('./../models/activity');
 const fs = require('fs');
 const pdf = require('pdfkit');
+const { hostname } = require('os');
 
 const curYear = new Date().getFullYear();
+const prevYear = curYear - 1;
 const startCurYear = new Date(curYear, 0, 1);
+const startPrevYear = new Date(prevYear, 0, 1);
 
 exports.find = (req, res, next) => {
+	const instance = platform.getPlatform(req.hostname);
+	//console.log(instance);
+	const minDate = instance.isKiso ? startPrevYear : startCurYear;
 	Registration.find({ userId: req.params.userId })
-	    .where('registrationDate').gte(startCurYear)
+	    .where('registrationDate').gte(minDate)
 		.populate({path: 'activityId', populate: {path: 'eventId'}})
 		.sort({ activityId: 1, lastNameChild: 1, firstNameChild: 1 })
 		.exec(function(err, reg) {
